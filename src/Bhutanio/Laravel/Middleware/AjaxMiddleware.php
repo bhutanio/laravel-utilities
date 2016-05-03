1bec9e30da5f793a88f1a3aa48ee50335b53f174
Abi Narayan <abixalmon@gmail.com>
2016-05-03 18:07:54 +0600

    added middlewares

---
+++ src/Bhutanio/Laravel/Middleware/AjaxMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bhutanio\Laravel\Middleware;
+
+use Closure;
+
+class AjaxMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (app()->environment() != 'local' && !$request->ajax()) {
+            return response('Not Allowed.', 405);
+        }
+
+        return $next($request);
+    }
+}
