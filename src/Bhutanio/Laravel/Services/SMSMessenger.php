275793e55918c82ef721ad698cf77a8edbbea8c4
Abi Narayan <abixalmon@users.noreply.github.com>
2017-06-18 03:37:24 +0600

    refactored

--- src/Bhutanio/Laravel/Services/SMSMessenger.php
+++ src/Bhutanio/Laravel/Services/SMSMessenger.php
@@ -4,36 +4,54 @@ namespace Bhutanio\Laravel\Services;
 
 use Nexmo\Client as Nexmo;
 use Nexmo\Client\Credentials\Basic;
+use Nexmo\Message\Message;
+use Nexmo\Message\Text;
 
 class SMSMessenger
 {
     private $nexmo;
 
+    private $from;
+
+    private $url;
+
     /**
      * Nexmo SMS Messenger.
      *
-     * @param string $api_key Nexmo API Key
-     * @param string $api_secret Nexmo API Secret
+     * @param string $api_key - Nexmo API Key
+     * @param string $api_secret - Nexmo API Secret
+     * @param null|string $from
+     * @param null|string $callback_url - URL to send callback response
      */
-    public function __construct($api_key, $api_secret)
+    public function __construct($api_key, $api_secret, $from, $callback_url = null)
     {
+        $this->from = $from;
+        $this->url = $callback_url;
+
         $this->nexmo = new Nexmo(new Basic($api_key, $api_secret));
     }
 
     /**
      * Send SMS Message
      *
-     * @param $from
-     * @param $to
-     * @param $message
-     * @return \Nexmo\Message\Message
+     * @param int $to
+     * @param string $message
+     * @param null|string $from
+     * @return Message
      */
-    public function send($from, $to, $message)
+    public function send($to, $message, $from = null)
     {
-        return $this->nexmo->message()->send([
-            'to'   => $to,
-            'from' => $from,
-            'text' => $message,
-        ]);
+        if (empty($from)) {
+            $from = $this->from;
+        }
+
+        $additional = [];
+        if ($this->url && filter_var($this->url, FILTER_VALIDATE_URL)) {
+            $additional['callback'] = $this->url;
+        }
+
+        $text = new Text($to, $from, $message, $additional);
+
+        return $this->nexmo->message()->send($text);
     }
 }
