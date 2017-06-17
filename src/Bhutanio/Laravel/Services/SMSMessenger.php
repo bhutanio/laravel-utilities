b30918e8373a41a02d9563937a70e2a73850dbd0
Abi Narayan <abixalmon@users.noreply.github.com>
2017-06-18 02:16:14 +0600

    phpdoc

--- src/Bhutanio/Laravel/Services/SMSMessenger.php
+++ src/Bhutanio/Laravel/Services/SMSMessenger.php
@@ -9,11 +9,25 @@ class SMSMessenger
 {
     private $nexmo;
 
+    /**
+     * Nexmo SMS Messenger.
+     *
+     * @param string $api_key Nexmo API Key
+     * @param string $api_secret Nexmo API Secret
+     */
     public function __construct($api_key, $api_secret)
     {
         $this->nexmo = new Nexmo(new Basic($api_key, $api_secret));
     }
 
+    /**
+     * Send SMS Message
+     *
+     * @param $from
+     * @param $to
+     * @param $message
+     * @return \Nexmo\Message\Message
+     */
     public function send($from, $to, $message)
     {
         return $this->nexmo->message()->send([
