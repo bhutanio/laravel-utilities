09b1d5991d9dfab6162b9ffe9458620d36e238a4
Abi Narayan <abixalmon@users.noreply.github.com>
2017-07-26 13:29:15 +0600

    use APP_NAME as site name

--- src/Bhutanio/Laravel/Services/MetaDataService.php
+++ src/Bhutanio/Laravel/Services/MetaDataService.php
@@ -2,19 +2,12 @@
 
 namespace Bhutanio\Laravel\Services;
 
-use Illuminate\Http\Request;
-
 class MetaDataService
 {
-    protected $meta_title, $page_title, $description, $canonical, $icon, $theme, $color, $request;
+    protected $site_name, $meta_title, $page_title, $description, $canonical, $icon, $theme, $color;
 
-    /**
-     * MetaDataService constructor.
-     * @param Request $request
-     */
-    public function __construct(Request $request)
+    public function __construct()
     {
-        $this->request = $request;
         $this->meta_title = $this->getDefaultTitle();
     }
 
@@ -23,7 +16,7 @@ class MetaDataService
         $this->pageTitle($page_title);
         $this->metaTitle($meta_title);
         if (empty($meta_title)) {
-            if ($page = $this->request->get('page')) {
+            if ($page = request()->get('page')) {
                 if ($page > 1) {
                     $page_title .= ' (Page ' . $page . ')';
                 }
@@ -62,19 +55,6 @@ class MetaDataService
         return $this->description;
     }
 
-    public function icon($icon = null)
-    {
-        if ($icon) {
-            $this->icon = $icon;
-        }
-
-        return $this->icon;
-    }
-
-    public function setTheme($theme = null, $color = null)
-    {
-    }
-
     public function canonical($url = null)
     {
         if ($url) {
@@ -84,8 +64,37 @@ class MetaDataService
         return $this->canonical;
     }
 
+    public function icon($icon = null)
+    {
+        if ($icon) {
+            $this->icon = $icon;
+        }
+
+        return $this->icon;
+    }
+
+    public function theme($theme = null)
+    {
+        if ($theme) {
+            $this->theme = $theme;
+        }
+
+        return $this->theme;
+    }
+
+    public function color($color = null)
+    {
+        if ($color) {
+            $this->color = $color;
+        }
+
+        return $this->color;
+    }
+
     private function getDefaultTitle()
     {
-        return env('SITE_NAME') ?: 'Site Name';
+        $this->site_name = env('SITE_NAME') ?: env('APP_NAME');
+
+        return $this->site_name ?: 'Site Name';
     }
 }
